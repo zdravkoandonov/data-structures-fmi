59a00b00d769dae1bb107abbd3a1ae93054d0fa7
Zdravko Andonov <zdravko.andonov.dev@gmail.com>
2015-07-17 17:18:59 +0300

    FIX: default value for the right child set to NULL

--- binary-tree/Node.h
+++ binary-tree/Node.h
@@ -3,7 +3,7 @@ struct Node
 {
 	int data;
 	Node *left, *right;
-	Node(int data, Node *left = NULL, Node *right) :
+	Node(int data, Node *left = NULL, Node *right = NULL) :
 		data(data), left(left), right(right)
 	{
 	}
