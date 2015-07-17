9b046eb4f07b2e77235a4daf06170a3f874f6465
Zdravko Andonov <zdravko.andonov.dev@gmail.com>
2015-07-17 17:19:54 +0300

    ADD: add(), find(), remove(), print() implemented

--- binary-tree/BinaryTree.cpp
+++ binary-tree/BinaryTree.cpp
@@ -1,5 +1,7 @@
+#include <iostream>
 #include "BinaryTree.h"
 
+using std::cout;
 
 BinaryTree::BinaryTree(int rootData)
 {
@@ -13,5 +15,96 @@ BinaryTree::~BinaryTree()
 
 void BinaryTree::add(int data)
 {
-
+	add(root, data);
 }
+
+void BinaryTree::add(Node *&curRoot, int data)
+{
+	if (curRoot == NULL)
+		curRoot = new Node(data);
+	else if (data > curRoot->data)
+		add(curRoot->right, data);
+	else
+		add(curRoot->left, data);
+}
+
+Node* BinaryTree::find(int data)
+{
+	return find(root, data);
+}
+
+Node* BinaryTree::find(Node *curRoot, int data)
+{
+	if (curRoot == NULL || curRoot->data == data)
+		return curRoot;
+
+	if (data > curRoot->data)
+		return find(curRoot->right, data);
+	else
+		return find(curRoot->left, data);
+}
+
+void BinaryTree::remove(int data)
+{
+	remove(root, data);
+}
+
+void BinaryTree::remove(Node *&curRoot, int data)
+{
+	if (curRoot != NULL)
+	{
+		if (data > curRoot->data)
+			remove(curRoot->right, data);
+		else if (data < curRoot->data)
+			remove(curRoot->left, data);
+		else
+			if (curRoot->left != NULL && curRoot->right != NULL)
+			{
+				Node *maxSmaller = curRoot->left;
+				while (maxSmaller->right != NULL)
+				{
+					maxSmaller = maxSmaller->right;
+				}
+				int numToReplace = maxSmaller->data;
+				remove(curRoot->left, numToReplace);
+				curRoot->data = numToReplace;
+			}
+			else if (curRoot->left != NULL)
+			{
+				Node *toDel = curRoot;
+				curRoot = curRoot->left;
+				delete toDel;
+			}
+			else if (curRoot->right != NULL)
+			{
+				Node *toDel = curRoot;
+				curRoot = curRoot->right;
+				delete toDel;
+			}
+			else
+			{
+				delete curRoot;
+				curRoot = NULL;
+			}
+	}
+}
+
+void BinaryTree::print()
+{
+	if (root != NULL)
+		print(root);
+}
+
+void BinaryTree::print(Node *curRoot)
+{
+	if (curRoot->left != NULL)
+	{
+		print(curRoot->left);
+	}
+	cout << curRoot->data << " ";
+	if (curRoot->right != NULL)
+	{
+		print(curRoot->right);
+	}
+}
+
--- binary-tree/BinaryTree.h
+++ binary-tree/BinaryTree.h
@@ -8,5 +8,13 @@ public:
 	BinaryTree(int rootData);
 	~BinaryTree();
 	void add(int);
+	Node* find(int);
+	void remove(int);
+	void print();
+private:
+	void add(Node *&curRoot, int);
+	Node* find(Node *curRoot, int);
+	void remove(Node *&curRoot, int);
+	void print(Node *curRoot);
 };
 
