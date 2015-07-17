773dbb0b204fe2b936e9adcfffd0987f0f47d868
Zdravko Andonov <zdravko.andonov.dev@gmail.com>
2015-07-17 17:21:02 +0300

    UPDATE: examples in the main function updated

--- binary-tree/main.cpp
+++ binary-tree/main.cpp
@@ -5,6 +5,29 @@ using namespace std;
 
 int main()
 {
+	BinaryTree tree(10);
+	tree.add(11);
+	tree.add(5);
+	tree.add(7);
+	tree.add(8);
+	tree.add(9);
+	tree.add(6);
+	tree.add(4);
+	tree.add(3);
+	tree.add(12);
+	tree.add(9);
+	tree.add(9);
+	tree.add(9);
+
+	tree.print();
+
+	cout << tree.find(3)->data << " " << tree.find(0) << endl;
+
+	tree.remove(10);
+
+	tree.print();
+
+	cin.get();
 
 	return 0;
 }
