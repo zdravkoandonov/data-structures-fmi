d5cdd90e80aaff125b4d42860b901366c0b3a532
Zdravko Andonov <zdravko.andonov.dev@gmail.com>
2015-10-23 11:25:18 +0300

    Removed unnecessary skipping reads, instead using seekg

--- bmp-reader/main.cpp
+++ bmp-reader/main.cpp
@@ -9,12 +9,15 @@ int main()
 	char* size = new char[4];
 	char* width = new char[4];
 	char* height = new char[4];
-	char* readSkipper = new char[100];
-	img.read(readSkipper, 2 * sizeof(char));
+
+	img.seekg(2 * sizeof(char), img.cur);
+	// bfSize
 	img.read(size, 4 * sizeof(char));
-	img.read(readSkipper, 8 * sizeof(char));
-	img.read(readSkipper, 4 * sizeof(char));
+
+	img.seekg(12 * sizeof(char), img.cur);
+	// biWidth
 	img.read(width, 4 * sizeof(char));
+	// biHeight
 	img.read(height, 4 * sizeof(char));
 
 	cout << *((int*)size) << endl;
